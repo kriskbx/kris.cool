5f44ab80ca8fafca2ec504f9c062ff57cfce86ed
kris <m@kris.cool>
2016-02-09 12:51:15 +0100

    added dev-deployment. added cache and build artificates to gitlab-ci

--- grunt/rsync.js
+++ grunt/rsync.js
@@ -4,7 +4,19 @@ module.exports = {
         options: {
             src: '<%= config.src %>',
             dest: process.env.CI_DEPLOYMENT_PATH,
-            host: process.env.CI_DEPLOYMENT_USER + '@' + process.env.CI_DEPLOYMENT_HOST,
+            host: process.env.CI_DEPLOYMENT_URI,
+            delete: false,
+            recursive: true,
+            exclude: [".git", "node_modules"],
+            args: ["--verbose --progress"]
+        }
+    },
+
+    test: {
+        options: {
+            src: '<%= config.src %>',
+            dest: process.env.CI_DEV_DEPLOYMENT_PATH,
+            host: process.env.CI_DEV_DEPLOYMENT_URI,
             delete: false,
             recursive: true,
             exclude: [".git", "node_modules"],
